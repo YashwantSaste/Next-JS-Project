1d0d2558687d592e05687f2e1b3ad2ad8ee49589
YashwantSaste <2021bec122@sggs.ac.in>
2024-02-05 20:54:42 +0530

    Fixing the auth-callback page

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 import { trpc } from '../_trpc/client';
 import { Loader2 } from 'lucide-react';
 
-const Page =  () => {
+const HomePage =  () => {
     const router = useRouter(); 
     const searchParams = useSearchParams();
     const origin = searchParams.get('origin');
@@ -42,4 +42,4 @@ const Page =  () => {
     );
 };
 
-export default Page;
+export default HomePage;
