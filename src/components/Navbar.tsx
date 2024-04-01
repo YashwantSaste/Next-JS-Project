2818146d878a785b337499f6e69b7cb7f08a4d94
YashwantSaste <2021bec122@sggs.ac.in>
2024-04-01 23:13:41 +0530

    Fixing the issue of unauthorized access of dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
 import { buttonVariants } from './ui/button'
-import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server'
+import { LoginLink, RegisterLink, getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { ArrowRight } from 'lucide-react'
+import UserLoginNav from './UserLogInNav'
 
-const Navbar = () => {
+const Navbar = async() => {
+    const { getUser } = getKindeServerSession()
+    const user = await getUser()
   return (
     <div className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 backdrop-blur-lg transition-all'>
         <MaxWidthWrapper>
@@ -17,14 +20,16 @@ const Navbar = () => {
                 {/* Mobile Navigation Bar */}
 
                 <div className='hidden items-center space-x-4 sm:flex'>
-                    <>
-                        <Link href='/pricing'
+                    {
+                        !user ? (
+                            <>
+                        <Link href='/dashboard'
                         className={buttonVariants({
                             variant:'ghost',
                             size:'sm',
                         })}
                         >
-                        Pricing
+                        Dashboard
                         </Link>
                         <LoginLink
                         className={buttonVariants({
@@ -44,6 +49,26 @@ const Navbar = () => {
                             <ArrowRight className='w-5 h-5 ml-1.5'/>
                         </RegisterLink>
                     </>
+                        ) : (
+                            <>
+                        <Link href='/pricing'
+                        className={buttonVariants({
+                            variant:'ghost',
+                            size:'sm',
+                        })}
+                        >
+                        Pricing
+                        </Link>
+                        <UserLoginNav name={
+                    !user.given_name || !user.family_name
+                      ? 'Your Account'
+                      : `${user.given_name} ${user.family_name}`
+                  }
+                  email={user.email ?? ''}
+                  imageUrl={user.picture ?? ''}/>
+                    </>
+                        )
+                    }
                     
                 </div>
             </div>
